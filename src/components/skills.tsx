4c6da4acd4801b96798d755617a02723a88fa139
arthurwillame5 <arthurwillame2005@gmail.com>
2024-06-21 11:35:46 -0300

    feat: update skills component with image alt text

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -4,8 +4,6 @@ import {
   Carousel,
   CarouselContent,
   CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
 } from "@/components/ui/carousel";
 import { skills } from "../app/info";
 import Image from "next/image";
@@ -13,7 +11,7 @@ import Autoplay from "embla-carousel-autoplay";
 
 const Skills = () => {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <main className="flex flex-col items-center justify-center">
       <h1 className="text-3xl pt-24 pb-14 font-semibold lg:text-4xl">Habilidades</h1>
       <Carousel
         plugins={[
@@ -28,18 +26,19 @@ const Skills = () => {
       >
         <CarouselContent>
           {skills.map((skill, index) => (
-            <CarouselItem key={index} className="basis-1/3">
+            <CarouselItem key={index} className="basis-1/3 text-center space-y-1">
               <Image
                 src={skill.src}
                 width={skill.width}
                 height={skill.height}
                 alt={skill.alt}
               />
+              <p className="truncate">{skill.alt}</p>
             </CarouselItem>
           ))}
         </CarouselContent>
       </Carousel>
-    </div>
+    </main>
   );
 };
 
