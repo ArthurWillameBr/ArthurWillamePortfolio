b452f4bb9c42e5afe3fc5f0f86f8d168feed5673
arthurwillame5 <arthurwillame2005@gmail.com>
2024-06-22 19:51:44 -0300

    refactor: Update projects component to use Framer Motion for animations

--- src/components/card-projects.tsx
+++ src/components/card-projects.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Card,
   CardContent,
@@ -17,71 +19,86 @@ import Image from "next/image";
 import { CarouselItem } from "../components/ui/carousel";
 import { ProjectsProps } from "@/app/data";
 
+import { motion } from "framer-motion";
 
-export function CardProjects({ projects }: { projects: ProjectsProps[]}) {
+export function CardProjects({ projects }: { projects: ProjectsProps[] }) {
   return (
     <>
       {projects.map((project, index) => (
         <CarouselItem key={index}>
-            <TooltipProvider>
-                  <Card className="bg-muted text-muted-foreground h-[300px]">
-                    <CardHeader>
-                      <div className="flex justify-between items-center">
-                        <div>
-                          <CardTitle className="pt-3 font-medium text-black dark:text-white">
-                            {project.name}
-                          </CardTitle>
-                        </div>
-                        <div className="flex items-center gap-4">
-                          <Tooltip>
-                            <TooltipTrigger asChild>
-                              <a href={project.deployLink}>
-                                <LinkIcon className="size-4 mt-2" />
-                              </a>
-                            </TooltipTrigger>
-                            <TooltipContent>Deploy</TooltipContent>
-                          </Tooltip>
-                          <Tooltip>
-                            <TooltipTrigger asChild>
-                              <a href={project.githubLink}>
-                                <Github className="mt-2"/>
-                              </a>
-                            </TooltipTrigger>
-                            <TooltipContent>Repositório</TooltipContent>
-                          </Tooltip>
-                          <Tooltip>
-                            <TooltipTrigger asChild>
-                              <a href={project.linkedinLink}>
-                                <Linkedin className="size-4 mt-2" />
-                              </a>
-                            </TooltipTrigger>
-                            <TooltipContent>Linkedin</TooltipContent>
-                          </Tooltip>
-                        </div>
-                      </div>
-                    </CardHeader>
-                    <hr className="mx-5 opacity-80" />
-                    <CardContent className="flex-1 items-center justify-center p-6 mb-5">
-                      <CardDescription>{project.description}</CardDescription>
-                    </CardContent>
-                    <hr className="mx-5 opacity-80" />
-                    <CardFooter className="flex items-center justify-center pt-1 gap-3">
-                      {project.stacks.map((stack, index) => (
-                        <Tooltip key={index}>
-                          <TooltipTrigger>
-                            <Image
-                              src={stack.src}
-                              width={stack.width}
-                              height={stack.height}
-                              alt={stack.alt}
-                            />
-                          </TooltipTrigger>
-                          <TooltipContent>{stack.alt}</TooltipContent>
-                        </Tooltip>
-                      ))}
-                    </CardFooter>
-                  </Card>
-            </TooltipProvider>
+          <TooltipProvider>
+            <motion.div
+              initial={{ opacity: 0, y: 100 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: 100 }}
+              transition={{ duration: 0.5 }}
+            >
+              <Card className="bg-muted text-muted-foreground h-[300px]">
+                <CardHeader>
+                  <div className="flex justify-between items-center">
+                    <div>
+                      <CardTitle className="pt-3 font-medium text-black dark:text-white">
+                        {project.name}
+                      </CardTitle>
+                    </div>
+                    <div className="flex items-center gap-4">
+                      <Tooltip>
+                        <TooltipTrigger asChild>
+                          <a href={project.deployLink}>
+                            <LinkIcon className="size-4 mt-2" />
+                          </a>
+                        </TooltipTrigger>
+                        <TooltipContent>Deploy</TooltipContent>
+                      </Tooltip>
+                      <Tooltip>
+                        <TooltipTrigger asChild>
+                          <a href={project.githubLink}>
+                            <Github className="mt-2" />
+                          </a>
+                        </TooltipTrigger>
+                        <TooltipContent>Repositório</TooltipContent>
+                      </Tooltip>
+                      <Tooltip>
+                        <TooltipTrigger asChild>
+                          <a href={project.linkedinLink}>
+                            <Linkedin className="size-4 mt-2" />
+                          </a>
+                        </TooltipTrigger>
+                        <TooltipContent>Linkedin</TooltipContent>
+                      </Tooltip>
+                    </div>
+                  </div>
+                </CardHeader>
+                <hr className="mx-5 opacity-80" />
+                <CardContent className="flex-1 items-center justify-center p-6 mb-5">
+                  <CardDescription>{project.description}</CardDescription>
+                </CardContent>
+                <hr className="mx-5 opacity-80" />
+                <CardFooter className="flex items-center justify-center pt-1 gap-3">
+                  {project.stacks.map((stack, index) => (
+                    <Tooltip key={index}>
+                      <TooltipTrigger>
+                        <motion.div
+                          initial={{ opacity: 0, scale: 0 }}
+                          whileInView={{ opacity: 1, scale: 1 }}
+                          exit={{ opacity: 0, scale: 0 }}
+                          transition={{ duration: 0.5, delay: index * 0.2 }}
+                        >
+                          <Image
+                            src={stack.src}
+                            width={stack.width}
+                            height={stack.height}
+                            alt={stack.alt}
+                          />
+                        </motion.div>
+                      </TooltipTrigger>
+                      <TooltipContent>{stack.alt}</TooltipContent>
+                    </Tooltip>
+                  ))}
+                </CardFooter>
+              </Card>
+            </motion.div>
+          </TooltipProvider>
         </CarouselItem>
       ))}
     </>
