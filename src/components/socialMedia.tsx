9c00c2c73de00928fc4239f11f592a6df891042e
arthurwillame5 <arthurwillame2005@gmail.com>
2024-06-22 19:06:12 -0300

    refactor: update social media component to use framer motion for animation

--- src/components/socialMedia.tsx
+++ src/components/socialMedia.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { socialLinks } from "@/app/data";
 import {
   Tooltip,
@@ -7,36 +9,47 @@ import {
 } from "../components/ui/tooltip";
 import { Github, Linkedin, Mail } from "lucide-react";
 
+import { motion } from "framer-motion"
+
 export function SocialMedia() {
   return (
-    <>
-      {socialLinks.map((socialLink) => {
-        return (
-          <TooltipProvider key={socialLink.id}>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <a
-                  className="group bg-primary px-3 py-2 rounded text-primary-foreground"
-                  href={socialLink.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {socialLink.name === "Linkedin" && (
-                    <Linkedin className="group-hover:scale-110 transition-all" />
-                  )}
-                  {socialLink.name === "GitHub" && (
-                    <Github className="group-hover:scale-110 transition-all" />
-                  )}
-                  {socialLink.name === "Email" && (
-                    <Mail className="group-hover:scale-110 transition-all" />
-                  )}
-                </a>
-              </TooltipTrigger>
-              <TooltipContent>{socialLink.name}</TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        );
-      })}
-    </>
+   <>
+   {socialLinks.map((socialLink, index) => {
+     return (
+       <motion.main
+       className="flex"
+       key={socialLink.id}
+       initial={{ opacity: 0, scale: 0}}
+       whileInView={{ opacity: 1, scale: 1}}
+       exit={{ opacity: 0, scale: 0 }}
+       transition={{ duration: 0.5, delay: index * 0.2}}
+       >
+       <TooltipProvider>
+         <Tooltip>
+           <TooltipTrigger asChild>
+             <a
+               className="group bg-primary px-3 py-2 rounded text-primary-foreground"
+               href={socialLink.link}
+               target="_blank"
+               rel="noopener noreferrer"
+             >
+               {socialLink.name === "Linkedin" && (
+                 <Linkedin className="group-hover:scale-110 transition-all" />
+               )}
+               {socialLink.name === "GitHub" && (
+                 <Github className="group-hover:scale-110 transition-all" />
+               )}
+               {socialLink.name === "Email" && (
+                 <Mail className="group-hover:scale-110 transition-all" />
+               )}
+             </a>
+           </TooltipTrigger>
+           <TooltipContent>{socialLink.name}</TooltipContent>
+         </Tooltip>
+       </TooltipProvider>
+       </motion.main>
+     );
+   })}
+   </>
   );
 }
