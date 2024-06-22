2019d6adbfa03c82a353d209df1029ad415fa8f3
arthurwillame5 <arthurwillame2005@gmail.com>
2024-06-22 20:53:57 -0300

    refactor: Update skills component to use Framer Motion for animations

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -9,18 +9,20 @@ import { skills } from "../app/data";
 import Image from "next/image";
 import Autoplay from "embla-carousel-autoplay";
 
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 const Skills = () => {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center">
       <motion.h1
-       className="text-3xl py-10 font-semibold lg:text-4xl tracking-tight"
-       initial={{ opacity: 0, x: -100 }}
-       whileInView={{ opacity: 1, x: 0}}
-       exit={{ opacity: 0, x: -100 }}
-       transition={{ duration: 0.5 }}
-       >Habilidades</motion.h1>
+        className="text-3xl py-10 font-semibold lg:text-4xl tracking-tight"
+        initial={{ opacity: 0, x: -100 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        exit={{ opacity: 0, x: -100 }}
+        transition={{ duration: 0.5 }}
+      >
+        Habilidades
+      </motion.h1>
       <Carousel
         plugins={[
           Autoplay({
@@ -32,19 +34,29 @@ const Skills = () => {
         }}
         className="w-full max-w-sm lg:max-w-lg"
       >
-        <CarouselContent>
-          {skills.map((skill, index) => (
-            <CarouselItem key={index} className="basis-1/3 text-center space-y-1">
-              <Image
-                src={skill.src}
-                width={skill.width}
-                height={skill.height}
-                alt={skill.alt}
-              />
-              <p className="truncate">{skill.alt}</p>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
+        <motion.div
+          initial={{ opacity: 0, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 100 }}
+          transition={{ duration: 0.5 }}
+        >
+          <CarouselContent>
+            {skills.map((skill, index) => (
+              <CarouselItem
+                key={index}
+                className="basis-1/3 text-center space-y-1"
+              >
+                <Image
+                  src={skill.src}
+                  width={skill.width}
+                  height={skill.height}
+                  alt={skill.alt}
+                />
+                <p className="truncate">{skill.alt}</p>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </motion.div>
       </Carousel>
     </main>
   );
