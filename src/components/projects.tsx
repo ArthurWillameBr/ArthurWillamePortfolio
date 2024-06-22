45f75400cefc4d3b9e414480d8909d4852f7e214
arthurwillame5 <arthurwillame2005@gmail.com>
2024-06-22 19:55:31 -0300

    refactor: Update projects component to use Framer Motion for animations

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -8,20 +8,27 @@ import {
 import { CardProjects } from "./card-projects";
 import { projects } from "@/app/data";
 
+import { motion } from "framer-motion";
 
 const Projects = () => {
   return (
     <main className="h-screen">
       <div className="flex items-center justify-center">
-        <h1 className="text-3xl pt-24 pb-14 font-semibold lg:text-4xl">
+        <motion.h1
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: -100 }}
+          transition={{ duration: 0.5 }}
+          className="text-3xl mb-10 font-semibold lg:text-4xl"
+        >
           Projetos
-        </h1>
+        </motion.h1>
       </div>
       <div className="flex justify-center items-center mx-auto px-8">
         <Carousel className="max-w-64">
-            <CarouselContent>
-                <CardProjects projects={projects} />
-            </CarouselContent>
+          <CarouselContent>
+            <CardProjects projects={projects} />
+          </CarouselContent>
           <CarouselNext />
           <CarouselPrevious />
         </Carousel>
