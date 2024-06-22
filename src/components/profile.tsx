fec54804ba0a8d3e1b3ea147cda37d7492405c3b
arthurwillame5 <arthurwillame2005@gmail.com>
2024-06-22 18:54:19 -0300

    refactor: Update profile component to adjust width and padding

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,9 +1,20 @@
+"use client";
+
 import { Button } from "./ui/button";
 import { SocialMedia } from "./socialMedia";
 
+import { motion } from "framer-motion";
+
 const Profile = () => {
   return (
-    <main  style={{ minHeight: 'calc(100vh - 188px)' }} className="flex flex-col items-center justify-center text-center gap-5 ">
+    <motion.div
+      style={{ minHeight: "calc(100vh - 188px)" }}
+      className="flex flex-col items-center justify-center text-center gap-5"
+      initial={{ opacity: 0, y: -100 }}
+      whileInView={{ opacity: 1, y: 0}}
+      exit={{ opacity: 0, y: -100 }}
+      transition={{ duration: 0.5 }}
+    >
       <h1 className="text-3xl lg:text-5xl font-bold tracking-tight">
         Olá, eu sou o <span className=""> Arthur Willame</span>
       </h1>
@@ -23,19 +34,19 @@ const Profile = () => {
       </div>
       <div className="mt-4">
         <div className="flex gap-2 items-center justify-center mb-4">
-          <SocialMedia/>
+          <SocialMedia />
         </div>
         <Button
           variant="outline"
-          className="relative inline-flex items-center overflow-hidden transition-all group border-2 border-primary"
+          className="relative inline-flex  items-center overflow-hidden transition-all group border-2 border-primary"
         >
           <span className="w-64 h-48 rounded bg-primary absolute right-96 bottom-1 translate-x-full ease-out duration-500 transition-all translate-y-full mb-12 group-hover:translate-x-96"></span>
-          <span className="relative w-full text-left transition-colors duration-300 ease-in-out text-black dark:text-white uppercase font-semibold ">
+          <span className="relative w-full text-left transition-colors duration-300 ease-in-out text-black group-hover:text-white dark:text-white uppercase font-semibold ">
             Curriculo
           </span>
         </Button>
       </div>
-    </main>
+    </motion.div>
   );
 };
 
