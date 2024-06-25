cccd04d1220d4f5fc825c03d41c71916b040a934
arthurwillame5 <arthurwillame2005@gmail.com>
2024-06-25 19:56:28 -0300

    feat: ensure the toast animation is the same on large and small screens

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,10 +67,20 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "slide-in-from-top-full": {
+          '0%': { transform: 'translateY(-100%)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+        "slide-out-to-top-full": {
+          '0%': { transform: 'translateY(0)', opacity: '1' },
+          '100%': { transform: 'translateY(-100%)', opacity: '0' },
+        },
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        "accordion-down": "accordion-down 0.1s ease-out",
+        "accordion-up": "accordion-up 0.1s ease-out",
+        "slide-in-from-top-full": "slide-in-from-top-full 0.2s ease-out forwards",
+        "slide-out-to-top-full": "slide-out-to-top-full 0.2s ease-in forwards",
       },
     },
   },
